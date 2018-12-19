96ea25b9c39ea4b83fde02a546f0d7cebd434439
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-19 13:18:12 -0600

    commit to show prof

--- mars/src/components/auth/Profile.jsx
+++ mars/src/components/auth/Profile.jsx
@@ -1,58 +1,154 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import {getProfile} from '../../services/auth'
 
-export default function Profile() {
-  return (
-    <div>
-         <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <Link to="/" className="navbar-item" >
-            <img src="/logo.png" alt="Logo"/>
-            </Link>
+ class ProfilePage extends Component {
 
-            <div role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            </div>
-        </div>
+    state={
+        user:{}
+    }
 
-        <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-            <Link to="/" className="navbar-item">
-                Mission to Mars
-            </Link>
-            <Link to="/" className="navbar-item">
-                Home
-            </Link>
+    componentWillMount(){
+        getProfile()
+            .then(user=>{
+                this.setState({user})
+            }).catch(error=>{
+                console.log(error)
+            })
+    }
 
-            </div>
+  render() {
+    const {user} = this.state
+    const loggedUser = JSON.parse(localStorage.getItem('loggedUser'))
+    if(!loggedUser) this.props.history.push('/login')
+    return (
+        <div>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+       <div className="navbar-brand">
+           <Link to="/" className="navbar-item" >
+           <img src="/logo.png" alt="Logo"/>
+           </Link>
 
-            <div className="navbar-end">
-            <div className="navbar-item">
-                <div className="buttons">
-                <Link to="/signup" className="button is-primary">
-                    <strong>Sign up</strong>
-                </Link>
-                <Link to="/login" className="button is-light">
-                    Log in
-                </Link>
-                </div>
-            </div>
-            </div>
-        </div>
-        </nav>
-      <div className="card">
-        <div className="card-content">
-            <div className="media">
+           <div role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+           <span aria-hidden="true"></span>
+           <span aria-hidden="true"></span>
+           <span aria-hidden="true"></span>
+           </div>
+       </div>
 
-            <div className="media-content">
-                <p className="title is-4">Name: Darth</p>
-                <p className="title is-4">Email: darth@darth.com</p>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-  )
+       <div id="navbarBasicExample" className="navbar-menu">
+           <div className="navbar-start">
+           <Link to="/" className="navbar-item">
+               Mission to Mars
+           </Link>
+           <Link to="/" className="navbar-item">
+               Home
+           </Link>
+
+           </div>
+
+           <div className="navbar-end">
+           <div className="navbar-item">
+               <div className="buttons">
+               <Link to="/signup" className="button is-primary">
+                   <strong>Sign up</strong>
+               </Link>
+               <Link to="/login" className="button is-light">
+                   Log in
+               </Link>
+               </div>
+           </div>
+           </div>
+       </div>
+       </nav>
+     <div className="card">
+       <div className="card-content">
+           <div className="media">
+
+           <div className="media-content">
+               <p className="title is-4">Name: {user.name} </p>
+               <p className="title is-4">Email: {user.email} </p>
+           </div>
+           </div>
+       </div>
+       </div>
+   </div>
+
+    )
+  }
 }
+export default ProfilePage
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import {Link} from 'react-router-dom'
+// import {getProfile} from '../../services/auth'
+
+
+// export default function Profile() {
+
+    
+
+//   return (
+//     <div>
+//          <nav className="navbar" role="navigation" aria-label="main navigation">
+//         <div className="navbar-brand">
+//             <Link to="/" className="navbar-item" >
+//             <img src="/logo.png" alt="Logo"/>
+//             </Link>
+
+//             <div role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+//             <span aria-hidden="true"></span>
+//             <span aria-hidden="true"></span>
+//             <span aria-hidden="true"></span>
+//             </div>
+//         </div>
+
+//         <div id="navbarBasicExample" className="navbar-menu">
+//             <div className="navbar-start">
+//             <Link to="/" className="navbar-item">
+//                 Mission to Mars
+//             </Link>
+//             <Link to="/" className="navbar-item">
+//                 Home
+//             </Link>
+
+//             </div>
+
+//             <div className="navbar-end">
+//             <div className="navbar-item">
+//                 <div className="buttons">
+//                 <Link to="/signup" className="button is-primary">
+//                     <strong>Sign up</strong>
+//                 </Link>
+//                 <Link to="/login" className="button is-light">
+//                     Log in
+//                 </Link>
+//                 </div>
+//             </div>
+//             </div>
+//         </div>
+//         </nav>
+//       <div className="card">
+//         <div className="card-content">
+//             <div className="media">
+
+//             <div className="media-content">
+//                 <p className="title is-4">Name: holi </p>
+//                 <p className="title is-4">Email: </p>
+//             </div>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+//   )
+// }
--- mars/src/services/auth.js
+++ mars/src/services/auth.js
@@ -1,24 +1,31 @@
 import axios from 'axios'
-const host = 'http://localhost:3000/auth'
+
+const host = 'http://localhost:3002/auth'
 
 //signup
 export const signup = (user) => {
-    return axios.post(host+"/signup", user, {})
-    .then(r => r.data)
-    .catch(e => e.response)
+    return axios.post(host+'/signup', user)
+        .then(r=>r.data)
+        .catch(e=>e.response)
 }
 
 //login
-export const login = (user) => {
-    return axios.post(host+"/login", user, {withCredentials: true})
-    .then(r => r.data)
-    .catch(e => e.response)
+export const login = (user) =>{
+    return axios.post(host+'/login', user,{withCredentials:true})
+        .then(r=>r.data)
+        .catch(e=>e.response)
 }
+
 //logout
 
 //profile
-export const profile = () => {
-    return axios.get(host+"/profile", {withCredentials:true})
-    .then(r => r.data)
-    .catch(e => e.response)
+
+export const getProfile = () =>{
+    return axios.get(host+'/profile',{withCredentials:true})
+        .then(r=>r.data)
+        .catch(e=>{
+            console.log(e)
+            return e.response
+            
+        })
 }
